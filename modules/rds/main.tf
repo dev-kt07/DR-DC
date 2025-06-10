85cbf6bcf64f7b76900d2a9b936dfa19f9dc38e9
dev-kt07 <krishnatonk939@gmail.com>
2025-06-10 16:02:11 +0530

    adding functionality

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,6 +1,15 @@
+resource "aws_db_subnet_group" "this" {
+  name       = "${replace(lower(var.name), "[^a-z0-9-]", "")}-subnet-group"
+  subnet_ids = var.db_subnet_ids
+
+  tags = {
+    Name = "${var.name}-subnet-group"
+  }
+}
+
 resource "aws_rds_cluster" "this" {
-  cluster_identifier     = "my-aurora-cluster"
-  snapshot_identifier    = "arn:aws:rds:us-east-2:xxxx:cluster-snapshot:copy-rds-snapshot"
+  cluster_identifier     = replace(lower(var.name), "[^a-z0-9-]", "")
+  snapshot_identifier    = var.snapshot_identifier
   engine                 = "aurora-mysql"
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = var.vpc_security_group_ids
@@ -8,10 +17,10 @@ resource "aws_rds_cluster" "this" {
 }
 
 resource "aws_rds_cluster_instance" "this" {
-  identifier              = "my-aurora-instance"
+  identifier              = "${replace(lower(var.name), "[^a-z0-9-]", "")}-instance"
   cluster_identifier      = aws_rds_cluster.this.id
-  instance_class          = "db.t3.medium"
+  instance_class          = var.db_instance_class
   engine                  = "aurora-mysql"
   db_subnet_group_name    = aws_db_subnet_group.this.name
-  publicly_accessible     = true
+  publicly_accessible     = false
 }
