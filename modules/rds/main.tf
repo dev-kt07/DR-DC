0950a677bc72a99ac1434744b10555b06ae2d303
dev-kt07 <krishnatonk939@gmail.com>
2025-06-10 15:45:28 +0530

    adding rds mcluster

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,23 +1,17 @@
-resource "aws_db_instance" "this" {
-  identifier              = "my-rds-database"
-  snapshot_identifier     = var.snapshot_identifier
-  instance_class          = var.db_instance_class
+resource "aws_rds_cluster" "this" {
+  cluster_identifier     = "my-aurora-cluster"
+  snapshot_identifier    = "arn:aws:rds:us-east-2:xxxx:cluster-snapshot:copy-rds-snapshot"
+  engine                 = "aurora-mysql"
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  skip_final_snapshot    = true
+}
+
+resource "aws_rds_cluster_instance" "this" {
+  identifier              = "my-aurora-instance"
+  cluster_identifier      = aws_rds_cluster.this.id
+  instance_class          = "db.t3.medium"
+  engine                  = "aurora-mysql"
   db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = var.vpc_security_group_ids
   publicly_accessible     = true
-  skip_final_snapshot     = true
-  apply_immediately       = true
-}
-
-resource "aws_db_subnet_group" "this" {
-  name       = "db-subnet-group"
-  subnet_ids = var.db_subnet_ids
-
-  tags = {
-    Name = "my-rds-subnet-group"
-  }
-}
-
-output "rds_endpoint" {
-  value = aws_db_instance.this.endpoint
 }
